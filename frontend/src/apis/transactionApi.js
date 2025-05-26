a9ea0bd7f2c075ae0e65025ca69d0226f7288e3f
mayankrsagar <mayankrsagar@gmail.com>
2025-05-26 18:30:21 +0530

    Add the render link

--- frontend/src/apis/transactionApi.js
+++ frontend/src/apis/transactionApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:5000/api/transactions";
+const baseUrl = "https://personal-finance-visualizer-j5m1.onrender.com/api/transactions";
 
 export const fetchAllTransactions = async () => {
   try {
